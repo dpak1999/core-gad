0c628c70db5d73c4333da1878c31d3a1d96e7649
dpak1999 <deepakkumardash313@gmail.com>
2020-10-10 17:05:56 +0530

    creating homescreen and product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>WELCOME TO CORE GAD</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
