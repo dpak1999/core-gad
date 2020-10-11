dc3f3255c238d7ef2feae63ad38a92cb0863e25a
dpak1999 <deepakkumardash313@gmail.com>
2020-10-11 18:18:37 +0530

    adding colors

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     });
 
-    console.log(`MOngoDB connected: ${conn.connection.host}`);
+    console.log(`MOngoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
