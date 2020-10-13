8632d2ee5815c32c4e73b44c8bed117c6aaeba3c
dpak1999 <deepakkumardash313@gmail.com>
2020-10-13 19:32:42 +0530

    adding custom error handlers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from "colors";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -16,6 +17,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
