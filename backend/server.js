db4068172ab71960834c85a5cea60abe4588f284
dpak1999 <deepakkumardash313@gmail.com>
2020-10-16 20:59:41 +0530

    creating user controller and route for authentication

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import colors from "colors";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -11,12 +13,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
