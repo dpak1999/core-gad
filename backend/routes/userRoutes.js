09c6455d5b1989ad5bf11f83065ee47491421978
dpak1999 <deepakkumardash313@gmail.com>
2020-10-16 23:06:17 +0530

    user registration

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getUserProfile, authUser } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+router.route("/").post(registerUser);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 
