4f571dec91d01af70ebfc8401190d62d2b05a00a
dpak1999 <deepakkumardash313@gmail.com>
2020-10-19 23:27:23 +0530

    creating order controller and routes

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CheckOutSteps from "../components/CheckOutSteps";
 import Message from "../components/Message";
