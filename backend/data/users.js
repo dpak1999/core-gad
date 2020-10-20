5a3747131cf94bbaf903cfecc8899378c788974e
dpak1999 <deepakkumardash313@gmail.com>
2020-10-20 20:34:16 +0530

    adding place order screen

--- backend/data/users.js
+++ backend/data/users.js
@@ -8,14 +8,44 @@ const users = [
     isAdmin: true,
   },
   {
-    name: "Kishan Raj",
-    email: "kishan@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: "Emily Zen",
+    email: "emily@example.com",
+    password: bcrypt.hashSync("emily#10", 10),
   },
   {
-    name: "Sambhav Sekhar Dalpat",
-    email: "sambhav@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    name: "Tom Collins",
+    email: "tom@example.com",
+    password: bcrypt.hashSync("tom#10", 10),
+  },
+  {
+    name: "Shawn Mendes",
+    email: "shawn@example.com",
+    password: bcrypt.hashSync("shawn#10", 10),
+  },
+  {
+    name: "Brad Pitt",
+    email: "brad@example.com",
+    password: bcrypt.hashSync("brad#10", 10),
+  },
+  {
+    name: "Billie Ellish",
+    email: "billie@example.com",
+    password: bcrypt.hashSync("billie#10", 10),
+  },
+  {
+    name: "Robert Downy",
+    email: "robert@example.com",
+    password: bcrypt.hashSync("robert#10", 10),
+  },
+  {
+    name: "Bradley Cooper",
+    email: "bradley@example.com",
+    password: bcrypt.hashSync("bradley#10", 10),
+  },
+  {
+    name: "Gal Gadot",
+    email: "gal@example.com",
+    password: bcrypt.hashSync("gal#10", 10),
   },
 ];
 
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { createOrder } from "../actions/orderActions";
 import CheckOutSteps from "../components/CheckOutSteps";
 import Message from "../components/Message";
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
+  const dispatch = useDispatch();
+
   const cart = useSelector((state) => state.cart);
 
   const addDecimals = (num) => {
@@ -26,8 +29,28 @@ const PlaceOrderScreen = () => {
     Number(cart.taxPrice)
   ).toFixed(2);
 
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order, success, error } = orderCreate;
+
+  useEffect(() => {
+    if (success) {
+      history.push(`/order/${order._id}`);
+    }
+    // eslint-disable-next-line
+  }, [history, success]);
+
   const placeOrderHandler = (e) => {
-    console.log("ordered");
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      })
+    );
   };
 
   return (
@@ -118,6 +141,10 @@ const PlaceOrderScreen = () => {
                   <Col>Total</Col>
                   <Col>${cart.totalPrice}</Col>
                 </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                {error && <Message variant="danger"> {error} </Message>}
               </ListGroup.Item>
 
               <ListGroup.Item>
