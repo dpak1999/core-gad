da2728c87084d6aad4851eb8ea02bdc473ffd88d
dpak1999 <deepakkumardash313@gmail.com>
2020-10-22 23:34:54 +0530

    creating all orders table on profile page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,6 +15,7 @@ import {
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderListMyReducer,
   orderPayReducer,
 } from "./reducers/orderReducers";
 
@@ -29,6 +30,7 @@ const reducer = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderMyList: orderListMyReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
