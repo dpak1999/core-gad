18ebc824dfdcea7cce7a21b4d7cfabcb77cc1541
dpak1999 <deepakkumardash313@gmail.com>
2020-10-23 20:37:23 +0530

    creating admin routes and endpoint

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -29,4 +29,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("You need to be an Admin");
+  }
+};
+
+export { protect, admin };
