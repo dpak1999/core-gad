70fc4c0e043f8e5767aafc55aaf9bff14c02436c
dpak1999 <deepakkumardash313@gmail.com>
2020-10-23 21:49:17 +0530

    fixing admin logout bug

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -6,15 +6,24 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { listUsers } from "../actions/userActions";
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push("/login");
+    }
+
     dispatch(listUsers());
-  }, [dispatch]);
+  }, [dispatch, history]);
 
   const deleteHandler = (id) => {
     console.log("deleted");
