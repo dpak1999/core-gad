de6cf41c439f39998bf9686f8da26c589a05cf4d
dpak1999 <deepakkumardash313@gmail.com>
2020-10-26 19:56:11 +0530

    creating image upload config

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,44 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png/;
+  const extname = filetypes.test(
+    path.extname(file.originalname).toLocaleLowerCase()
+  );
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Please a valid type of jpeg,jpg or png!!");
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+export default router;
