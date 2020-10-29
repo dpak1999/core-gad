e4a74f634c9e1c52100fd2c28fd60669029c7c94
dpak1999 <deepakkumardash313@gmail.com>
2020-10-29 20:24:39 +0530

    adding helmet for dynamic title

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description}></meta>
+      <meta name="keywords" content={keywords}></meta>
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Welcome to coregad",
+  description: "We sell the best products for cheap",
+  keywords: "electronics, buy electronics, cheap electroincs",
+};
+
+export default Meta;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,7 +6,9 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import ProductCarousel from "../components/ProductCarousel";
 import Paginate from "../components/Paginate";
+import Meta from "../components/Meta";
 import { listProducts } from "../actions/productActions";
+import { Link } from "react-router-dom";
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword;
@@ -24,7 +26,14 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
-      {!keyword && <ProductCarousel />}
+      <Meta />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to="/" className="btn btn-light">
+          Go Back
+        </Link>
+      )}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
