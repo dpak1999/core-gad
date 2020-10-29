93dd468e354d9c3689d8b58784f45ce79d40f6fe
dpak1999 <deepakkumardash313@gmail.com>
2020-10-29 17:45:49 +0530

    installing morgan and adding reviews endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,11 +5,13 @@ import {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 router
   .route("/:id")
   .get(getProductById)
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
@@ -16,6 +17,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
