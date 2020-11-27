7bb6905e61287e07fb30b18361663bc4c700e359
Kishan Raj <52540375+kishanRaj10@users.noreply.github.com>
2020-11-27 22:53:02 +0530

    Update PaymentScreen.js

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Form, Button, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
-import CheckoutSteps from "../components/CheckoutSteps";
+import CheckoutSteps from "../components/CheckOutSteps";
 import { savePaymentMethod } from "../actions/cartActions";
 
 const PaymentScreen = ({ history }) => {
