570aa7ccd1782e1789b4081965e3485288d7df10
Kishan Raj <52540375+kishanRaj10@users.noreply.github.com>
2020-11-27 22:39:30 +0530

    fixed the errors in the PaymentScreen.js

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { Form, Button, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { savePaymentMethod } from "../actions/cartActions";
-import CheckOutSteps from "../components/CheckOutSteps";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { savePaymentMethod } from "../actions/cartActions";
 
 const PaymentScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart);
   const { shippingAddress } = cart;
-  if (!shippingAddress) {
+
+  if (!shippingAddress.address) {
     history.push("/shipping");
   }
 
@@ -24,7 +25,7 @@ const PaymentScreen = ({ history }) => {
 
   return (
     <FormContainer>
-      <CheckOutSteps step1 step2 step3 />
+      <CheckoutSteps step1 step2 step3 />
       <h1>Payment Method</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group>
